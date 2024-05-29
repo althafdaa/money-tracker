aa6ae7e45fe297d2857282c89d1be7b17387444e
althafdaa <althafdemiandraakbar@gmail.com>
2024-05-29 18:26:26 +0700

    check again with new account

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,7 +11,7 @@ func (s *FiberServer) RegisterFiberRoutes() {
 
 func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
 	resp := fiber.Map{
-		"message": "Hello Althaf",
+		"message": "Hello Check",
 	}
 
 	return c.JSON(resp)
